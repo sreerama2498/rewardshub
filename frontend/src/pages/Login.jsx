3c51bc89409a90d1563a45c0d003e4936a21f435
root <root@sreeram>
2026-06-03 16:50:48 +0530

    login page is fixed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -22,7 +22,8 @@ export default function Login() {
           password
         }
       );
-
+      console.log("LOGIN RESPONSE:");
+      console.log(response.data);
       localStorage.setItem(
         "token",
         response.data.access_token
